0e56062d330ec6a3662e3850ef5e66d97b7830d5
samfrexz619 <samfrexz69@gmail.com>
2023-03-31 18:04:29 +0100

    update/refactored the code to dynamically render the links instead of being hardcoded

--- src/components/Sidenav.tsx
+++ src/components/Sidenav.tsx
@@ -1,41 +1,21 @@
-import { useState } from 'react';
+import { useContext } from 'react';
+import { LinkContext } from '../context/LinkContext';
 import { NavLink } from 'react-router-dom';
 import '../App.scss'
 import SidenavIcon from './SidenavIcon';
 
-type LinkProps = {
-  name: string;
-  id: number;
-  path: string;
-}
-
-const linkItems = [
-  {
-    name: 'home',
-    id: 1,
-    path: '/'
-  },
-  {
-    name: 'puns',
-    id: 2,
-    path: '#'
-  },
-  {
-    name: 'polls',
-    id: 3,
-    path: '#'
-  },
-]
 
 const SideNav = () => {
-  const [links, setLinks] = useState<LinkProps[]>(linkItems)
+
+  const links = useContext(LinkContext)
+  
   return ( 
     <aside className='side-nav py-7'>
         <ul className='w-full'>
           {
             links.map(link => (
               <li key={link.id} className='py-3'>
-                <NavLink to={link.path} className={`flex items-center text-text`}>
+                <NavLink to={link.path} className={`flex items-center capitalize`}>
                   <SidenavIcon icon={link.name} />
                   <span className='px-3'>{link.name}</span>
                 </NavLink>
